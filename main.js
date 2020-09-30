b8b9c42727768deeaa6465a165a82309403cfa11
shahidbangash <shahidbangashsi94@gmail.com>
2020-10-01 02:35:29 +0500

    image updates when hover over color ..

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
 // const product = 'Socks'
 
 const app = Vue.createApp({
-    data() {
-        return {
-            'product' :'socks',
-            'discription': "This is product discription",
-            'image': './assets/images/socks_blue.jpg',
-        }
+  data() {
+    return {
+      product: "Socks",
+      discription: "This is product discription",
+      inventory: 10,
+      onsale: true,
+      cart: 0,
+      image: "./assets/images/socks_blue.jpg",
+      color: ["green", "blue"],
+      description: ["50% Cotton", "50% Polyster"],
+      item_details: [
+        {
+          id: 123,
+          color : 'blue',
+          image: "./assets/images/socks_blue.jpg",
+        },
+
+         {
+          id: 124,
+          color : 'green',
+          image: "./assets/images/socks_green.jpg",
+        },
+    ],
+    };
+  },
+  methods: {
+    addtocart() {
+      this.cart += 1;
     },
+    removeFromCart() {
+        this.cart -= 1;
+    },
+    changeImage(image){
+        console.log(this.cart);
+        // console.log(id);
+        this.image = image;
+    }
+  },
 });
